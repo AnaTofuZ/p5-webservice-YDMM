8a71b2e083db43ef34834ca8703231c4a249c16e
AnaTofuZ <anatofuz@gmail.com>
2017-10-16 14:48:47 +0900

    add newFile

---
+++ lib/WebService/YDMM.pm
@@ -0,0 +1,39 @@
+package WebService::YDMM;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::YDMM - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::YDMM;
+
+=head1 DESCRIPTION
+
+WebService::YDMM is ...
+
+=head1 LICENSE
+
+Copyright (C) AnaTofuZ.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+AnaTofuZ E<lt>anatofuz@gmail.comE<gt>
+
+=cut
+
